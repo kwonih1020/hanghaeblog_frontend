97e1c4df1abdb15043870f1e84348b51dea2c3d3
ParkYongWon <dyddnjs1209@naver.com>
2022-08-13 03:26:54 +0900

    get

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import GlobalLayout from "../global/GlobalLayout";
+import { useSelector, useDispatch} from "react-redux";
+import { getContentAsync } from "../redux/modules/contentSlice";
+
+
 
 const HomePage = () => {
-  return <GlobalLayout>메인 페이지.</GlobalLayout>;
+
+  const dispatch = useDispatch();
+  const content = useSelector((state)=>state.content)
+  const [check,setCheck] = useState(false)
+  console.log(content)
+
+
+  useEffect(()=>{
+    dispatch(getContentAsync());
+  },[check])
+
+
+
+
+
+
+  return (
+  <GlobalLayout>메인 페이지.
+    <div>
+
+    </div>
+
+
+
+
+  </GlobalLayout>
+  )
 };
 
 export default HomePage;
--- src/redux/modules/contentSlice.js
+++ src/redux/modules/contentSlice.js
@@ -1,13 +1,41 @@
-// // eslint-disable-next-line
+// src/redux/modules/contentSlice.js
 
-// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// const contentSlice = createSlice({
-//   name: "contentSlice",
-//   // initialState,
-//   // reducers: {},
-//   // extraReducers: {},
-// });
+const initialState = [{
+  
+      id: 1,
+      title: "기본 입니다",
+      body: "기본 내용",
+    
+}];
 
-// export default contentSlice.reducer;
+export const getContentAsync = createAsyncThunk(
+    "content/getContent", 
+    async () => {
+  const response = await axios.get("http://localhost:3001/content");
+  return response.data
+});
+
+export const contentSlice = createSlice({
+  name: "content",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    // [getContent.pending]: (state) => {
+    //   state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    // },
+    [getContentAsync.fulfilled]: (state, {payload}) => {
+      console.log("getContent");
+      return [...payload]; // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+    },
+    // [getContent.rejected]: (state, action) => {
+    //   state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+    //   state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
+    // },
+  },
+});
+
+export const {} = contentSlice.actions;
+export default contentSlice.reducer;
