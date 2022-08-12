fd64398f33e45ef96ae0efd7763366cb44973201
ParkYongWon <dyddnjs1209@naver.com>
2022-08-12 21:59:53 +0900

    first

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GlobalLayout from "../global/GlobalLayout";
 
 const HomePage = () => {
-  return <GlobalLayout>메인 페이지입니다.</GlobalLayout>;
+  return <GlobalLayout>메인 페이지.</GlobalLayout>;
 };
 
 export default HomePage;
