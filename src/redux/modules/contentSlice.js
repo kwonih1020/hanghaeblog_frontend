408d035d4198da74622dfa5f4cf4ad13a113aaac
kwonih1020 <kwonih1020@gmail.com>
2022-08-15 19:04:42 +0900

    코드 prettier 읽기 수정

--- src/redux/modules/contentSlice.js
+++ src/redux/modules/contentSlice.js
@@ -4,44 +4,35 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  list: []
-}
-
-  
-      
-    
-
+  list: [],
+};
 
 export const getContent = createAsyncThunk(
-    "content/getContent", 
-    async (kim, thunkAPI) => {
-      try {
-  const { data } = await axios.get("http://localhost:3001/content");
-  return thunkAPI.fulfillWithValue(data);
-} catch (e) {
-  return thunkAPI.rejectWithValue(e.code);
-}
-}
+  "content/getContent",
+  async (kim, thunkAPI) => {
+    try {
+      const { data } = await axios.get("http://localhost:3001/content");
+      return thunkAPI.fulfillWithValue(data);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.code);
+    }
+  }
 );
-
 
 export const postContent = createAsyncThunk(
   "content/postContent",
   async (args, thunkAPI) => {
     try {
-    const { data } = await axios.post("http://localhost:3001/content", args);
-    console.log("여기까지오냐?")
-    return thunkAPI.fulfillWithValue(data);
+      const { data } = await axios.post("http://localhost:3001/content", args);
+      console.log("여기까지오냐?");
+      return thunkAPI.fulfillWithValue(data);
     } catch (e) {
       return thunkAPI.rejectWithValue(e);
     }
   }
 );
 
-
-
 // console.log(getContent)
-
 
 export const contentSlice = createSlice({
   name: "contentSlice",
@@ -59,7 +50,7 @@ export const contentSlice = createSlice({
       state.list.push(action.payload);
       // console.log(state)
 
-       // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+      // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
     },
     // [getContent.rejected]: (state, action) => {
     //   state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
